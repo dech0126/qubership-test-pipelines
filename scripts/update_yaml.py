e511610b0b9cd1f81e5dfa18d7501a8b03a4e61f
Denis Chernykh <denis.chernykh@netcracker.com>
2026-03-06 16:49:28 +0500

    style: fix quotes, and \n after update from original main

--- scripts/update_yaml.py
+++ scripts/update_yaml.py
@@ -5,7 +5,7 @@ import yaml
 
 
 def set_new_value(data, path, value):
-    keys = path.split('/')
+    keys = path.split("/")
     current = data
     for key in keys[:-1]:
         if key not in current:
@@ -22,29 +22,29 @@ def main(args_):
         with open(path_to_file) as f:
             data_ = yaml.safe_load(f)
     except yaml.YAMLError as exc:
-        print(f'YAML loading error: {exc}')
+        print(f"YAML loading error: {exc}")
         sys.exit(1)
     except FileNotFoundError:
         print(f"File not found: {path_to_file}")
-        sys.exit(1) 
-        sys.exit(1) 
+        sys.exit(1)
+        sys.exit(1)
     set_new_value(data_, args_.path, args_.value)
-    with open(path_to_file, 'w') as f:
+    with open(path_to_file, "w") as f:
         yaml.dump(data_, f, default_flow_style=False, sort_keys=False)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description='Script for changing values in YAML files'
+        description="Script for changing values in YAML files"
     )
-    parser.add_argument('-f', '--file', required=True, help='Path to YAML file')
+    parser.add_argument("-f", "--file", required=True, help="Path to YAML file")
     parser.add_argument(
-        '-p',
-        '--path',
+        "-p",
+        "--path",
         required=True,
-        help='Path to value to be changed (format: key1/key2/key3)',
+        help="Path to value to be changed (format: key1/key2/key3)",
     )
-    parser.add_argument('-v', '--value', required=True, help='New value')
+    parser.add_argument("-v", "--value", required=True, help="New value")
 
     args = parser.parse_args()
     main(args)
